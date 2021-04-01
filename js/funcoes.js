864957fd6ec3d1081bec81288cc23482f63e7bc5
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-04-01 20:59:31 -0300

    colocando o numero secreto de uma maneira aleatoria

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,5 +1,7 @@
-//caso se digite este numero estaria correto
-let NumeroSecreto = 7;
+
+///o parse inte gera um numero inteiro e o math ramdom faz ser aleatorio a multiplicação por 10 é para que seja da primeira casa 
+let NumeroSecreto = parseInt(Math.random() * 10);
+//total de tentativas para acertar
 let tentativas = 3;
 //while seria os laços de repetição 
 while (tentativas > 0) {
@@ -10,9 +12,11 @@ while (tentativas > 0) {
         alert("Parebens, voce acertou!");
         //break é para se acertar o codigo parar
        break;
+       // se o numero secreto for maior que o digitado pelo usuario aparece a mensagem notificando
     }else if(NumeroSecreto > chute){
         alert("o numero secreto é maior que o chute");
         tentativas = tentativas - 1;
+        // se for menor a opção aparece como menor 
     }else if(NumeroSecreto < chute){
         alert("o numero secreto é menor que o chute");
         tentativas = tentativas - 1;
